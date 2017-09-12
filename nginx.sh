37875d512b608f4eefc04f8f40b3c373394b0520
Caripson <31686838+Caripson@users.noreply.github.com>
2017-09-12 16:56:03 +0200

    update

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo rm -rf /etc/nginx/sites-available/default
-sudo cp /tmp/HLS-monitoring/etc/nginx/sites-available/default /etc/nginx/sites-available/default
+sudo cp /tmp/HTTP-monitoring/etc/nginx/sites-available/default /etc/nginx/sites-available/default
