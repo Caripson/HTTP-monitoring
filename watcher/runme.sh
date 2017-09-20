764f3cf17b135a3ca2b23f35aacab43e1c577123
Caripson <31686838+Caripson@users.noreply.github.com>
2017-09-20 10:20:05 +0200

    delete old log files
    delete old log files older then 5 days .

--- watcher/runme.sh
+++ watcher/runme.sh
@@ -4,6 +4,9 @@ hls-check()
 {
 
 sURL=http://www.google.com/;timestamp=$(date +%Y%m%d%H%M);output=$(/usr/bin/curl -m 120 -Lo /dev/null -skw "%{time_connect}\t%{time_namelookup}\t%{time_pretransfer}\t%{time_starttransfer}\t%{time_redirect}\t%{time_total}\n" $sURL);LOGFILE=/home/$timestamp.check.log;echo "$(date "+%Y-%m-%d %T") "$output >> $LOGFILE
+find /home/*.log -mtime +5 -exec rm {} \;
+
+
 
 }
 
